207287803b88f20a60f2e6ec9980d8bbb5775768
papi-boot <papi8261@gmail.com>
2022-01-03 06:59:23 +0800

    change scoket config

--- src/api/useSocket.js
+++ src/api/useSocket.js
@@ -3,5 +3,5 @@ const socketURL =
   process.env.NODE_ENV === "production"
     ? "https://imshy-api.herokuapp.com"
     : "http://localhost:3030";
-const socket = io(socketURL);
+const socket = io(socketURL, { withCredentials: true, transports: ["websocket"] });
 export const useSocket = () => socket;
