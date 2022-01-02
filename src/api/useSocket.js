7efe14d12893af7ee3f308714a3bcea1b9825f11
papi-boot <papi8261@gmail.com>
2022-01-02 16:47:39 +0800

    change socket url

--- src/api/useSocket.js
+++ src/api/useSocket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const socketURL =
   process.env.NODE_ENV === "production"
-    ? "https://im-shy.herokuapp.com"
+    ? "https://imshy-api.herokuapp.com/"
     : "http://localhost:3030";
 const socket = io(socketURL, { withCredentials: true });
 export const useSocket = () => socket;
