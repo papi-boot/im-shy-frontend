29804badadb40d9d9423142543e19c37c05cd092
papi-boot <papi8261@gmail.com>
2022-01-02 19:23:47 +0800

    remove withCredentials

--- src/api/useSocket.js
+++ src/api/useSocket.js
@@ -3,5 +3,5 @@ const socketURL =
   process.env.NODE_ENV === "production"
     ? "https://imshy-api.herokuapp.com"
     : "http://localhost:3030";
-const socket = io(socketURL, { withCredentials: true,  });
+const socket = io(socketURL);
 export const useSocket = () => socket;
